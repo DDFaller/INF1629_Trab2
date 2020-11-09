f4378c5d369ff77c66de17637987ff7b4a18a0b4
Daniel Faller <danielmfaller@hotmail.com>
2020-11-09 14:25:25 -0300

    Changing setting file

--- term_frequency/term_frequency/settings.py
+++ term_frequency/term_frequency/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'atjzrj#e98iui1%l3u1(^qqnc)a(6&gfyl=0-(#m(p6umu_u8x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rocky-taiga-05060','127.0.0.1']
 
 
 # Application definition
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
