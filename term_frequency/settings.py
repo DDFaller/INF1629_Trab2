3acbc8c0b33095e8f20b85d8b06ea6d659e52602
DDFaller <danielmfaller@hotmail.com>
2020-11-19 15:40:49 -0300

    Fixing templates

--- term_frequency/settings.py
+++ term_frequency/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_DIR = (os.path.join(SETTINGS_PATH,'templates'))
 
 # Quick-start development settings - unsuitable for production
