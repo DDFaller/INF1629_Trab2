0373161109ca9d3fb1d821bce38cfdd1fc0932c7
DDFaller <danielmfaller@hotmail.com>
2020-11-19 18:38:09 -0300

    Testing bootstrap

--- term_frequency/settings.py
+++ term_frequency/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(os.path.abspath(os.path.dirname(__file__)),'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
