e6ed6eafed5c6440a7f2bd55426f672fedbc778c
DDFaller <danielmfaller@hotmail.com>
2020-11-19 18:30:41 -0300

    Adding bootstrap

--- term_frequency/settings.py
+++ term_frequency/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'term_frequency.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates/'),os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
