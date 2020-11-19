c1757d500c657ecb0f02502ef0e033ec2ecc6732
DDFaller <danielmfaller@hotmail.com>
2020-11-19 14:56:55 -0300

    Adding upload.html

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World!")  
+    return HttpResponse("Hello, World!")
+
+def upload(request):
+    return render(request,'upload.html')
 # Create your views here.
