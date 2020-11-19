9152d9da0fdf92ec140f09ae856fe7aed3b4e90b
DDFaller <danielmfaller@hotmail.com>
2020-11-19 15:31:45 -0300

    Fixing urls

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def index(request):
     return HttpResponse("Hello, World!")
--- term_frequency/urls.py
+++ term_frequency/urls.py
@@ -18,9 +18,9 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('app/', include("firstapp.urls")),
+    path('upload',views.upload, name='upload'),
     path('admin/', admin.site.urls),
 ]
 
