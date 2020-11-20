1364fa883e49367658d40b8da43516d181488771
DDFaller <danielmfaller@hotmail.com>
2020-11-20 07:32:35 -0300

    Receiving file

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from forms import FileFieldForm
+from . import forms
 from functions import handle_uploaded_file
 
 def index(request):
@@ -9,13 +9,13 @@ def index(request):
     return render(request,"base.html")
 
 def formSubmission(request):
-    form = FileFieldForm()
+    form = forms.FileFieldForm()
     if request.method == "POST":
-        form = FileFieldForm(request.POST,request.FILES)
+        form = forms.FileFieldForm(request.POST,request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
             return HttpResponse("File uploaded sucessfully")
         else:
-            form = FileFieldForm()
+            form = forms.FileFieldForm()
     return render(request,'home.html',{'form': form})
     #return render(request,'upload.html')
