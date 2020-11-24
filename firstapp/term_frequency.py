511b3304f812419cdfa170e4099a2f7926e97dda
DDFaller <danielmfaller@hotmail.com>
2020-11-24 19:34:32 -0300

    Fixing get

--- firstapp/term_frequency.py
+++ firstapp/term_frequency.py
@@ -12,11 +12,11 @@ def touchopen(filename, *args, **kwargs):
 
 class term_frequency():
     def __init__(self,stopwords_file,content_file):
-        self.stopwords_file = stopwords_file.open()#'../stop_words.txt'
+        self.stopwords_file = open(stopwords_file, 'r')#'../stop_words.txt'
         self.stopwords = [self.stopwords_file.read(1024).split(',')]
         self.stopwords_file.close()
         self.word_freqs = touchopen('word_freqs', 'rb+')
-        self.data_file = content_file.open()
+        self.data_file = open(content_file,'rb+')
 
 
     #Recebe um arquivo e iterando suas linhas registra as frequências das palavras
