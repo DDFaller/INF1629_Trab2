54953513bcfd734641776ff4e146ef79ce5cc1ba
DDFaller <danielmfaller@hotmail.com>
2020-11-24 18:14:12 -0300

    Fixing get

--- firstapp/views.py
+++ firstapp/views.py
@@ -11,24 +11,24 @@ def index(request):
 def upload(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
-        return render(request, 'base.html')
+        return render(request, 'home.html')
     return render(request,'upload.html')
 
-def get(request):
-    context = RequestContext(request)
-    context_dict = {}
-    # Key statement needs to be added
-    context_dict.update(csrf(request))
-    return render_to_response("upload.html", context_dict, context)
+#def get(request):
+#    context = RequestContext(request)
+#    context_dict = {}
+#    # Key statement needs to be added
+#    context_dict.update(csrf(request))
+#    return render_to_response("upload.html", context_dict, context)
 
-def formSubmission(request):
-    form = forms.FileFieldForm()
-    if request.method == "POST":
-        form = forms.FileFieldForm(request.POST,request.FILES)
-        if form.is_valid():
-            functions.handle_uploaded_file(request.FILES['file'])
-            return HttpResponse("File uploaded sucessfully")
-        else:
-            form = forms.FileFieldForm()
-    return render(request,'home.html',{'form': form})
+#def formSubmission(request):
+#    form = forms.FileFieldForm()
+#    if request.method == "POST":
+#        form = forms.FileFieldForm(request.POST,request.FILES)
+#        if form.is_valid():
+#            functions.handle_uploaded_file(request.FILES['file'])
+#            return HttpResponse("File uploaded sucessfully")
+#        else:
+#            form = forms.FileFieldForm()
+#    return render(request,'home.html',{'form': form})
     #return render(request,'upload.html')
